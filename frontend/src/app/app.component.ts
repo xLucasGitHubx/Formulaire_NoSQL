3631478dadf14345958b711369a3e035d96dfc71
xLucasGitHubx <130939836+xLucasGitHubx@users.noreply.github.com>
2025-06-05 13:37:01 +0200

    feat: enhance survey builder

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,38 +2,99 @@
 
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators
+} from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { FooterComponent } from './component/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    FormsModule,
-    HttpClientModule
+    ReactiveFormsModule,
+    HttpClientModule,
+    NavbarComponent,
+    FooterComponent
   ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Formulaire Sondage';
-  formData = {
-    nom: '',
-    platPrefere: ''
-  };
+  title = 'Créateur de Sondage';
+  surveyForm: FormGroup;
 
-  constructor(private http: HttpClient) {}
+  constructor(private fb: FormBuilder, private http: HttpClient) {
+    this.surveyForm = this.fb.group({
+      surveyName: ['', Validators.required],
+      questions: this.fb.array([this.createQuestion()])
+    });
+  }
+
+  private createQuestion(): FormGroup {
+    return this.fb.group({
+      title: ['', Validators.required],
+      type: ['text', Validators.required],
+      options: this.fb.array([])
+    });
+  }
+
+  get questions(): FormArray {
+    return this.surveyForm.get('questions') as FormArray;
+  }
+
+  addQuestion(): void {
+    this.questions.push(this.createQuestion());
+  }
+
+  removeQuestion(index: number): void {
+    this.questions.removeAt(index);
+    if (this.questions.length === 0) {
+      this.addQuestion();
+    }
+  }
+
+  getOptions(questionIndex: number): FormArray {
+    return this.questions.at(questionIndex).get('options') as FormArray;
+  }
+
+  addOption(questionIndex: number): void {
+    this.getOptions(questionIndex).push(this.fb.control('', Validators.required));
+  }
+
+  removeOption(questionIndex: number, optionIndex: number): void {
+    this.getOptions(questionIndex).removeAt(optionIndex);
+  }
+
+  onTypeChange(questionIndex: number): void {
+    const q = this.questions.at(questionIndex);
+    if (q.get('type')?.value !== 'choice') {
+      this.getOptions(questionIndex).clear();
+    }
+  }
 
   onSubmit(): void {
+    if (this.surveyForm.invalid) {
+      return;
+    }
+
     this.http
-      .post('http://localhost:9000/api/reponses', this.formData)
+      .post('http://localhost:9000/api/reponses', this.surveyForm.value)
       .subscribe({
         next: res => {
-          console.log('Réponse envoyée !', res);
-          // Réinitialiser le formulaire si besoin
-          this.formData = { nom: '', platPrefere: '' };
+          console.log('Sondage enregistré !', res);
+          this.surveyForm.reset();
+          // remettre une question vide
+          this.questions.clear();
+          this.addQuestion();
         },
-        error: err => console.error('Erreur d’envoi :', err)
+        error: err => console.error('Erreur d\u2019envoi :', err)
       });
   }
 }
