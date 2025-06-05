33d37252f86c854ca7f7c97ef67423df8d91a911
xLucasGitHubx <130939836+xLucasGitHubx@users.noreply.github.com>
2025-06-05 11:58:07 +0200

    feat(frontend): enhance survey creator UI

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,38 +2,72 @@
 
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators
+} from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { FooterComponent } from './component/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    FormsModule,
-    HttpClientModule
+    ReactiveFormsModule,
+    HttpClientModule,
+    NavbarComponent,
+    FooterComponent
   ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Formulaire Sondage';
-  formData = {
-    nom: '',
-    platPrefere: ''
-  };
+  title = 'Créateur de Sondage';
+  surveyForm: FormGroup;
 
-  constructor(private http: HttpClient) {}
+  constructor(private fb: FormBuilder, private http: HttpClient) {
+    this.surveyForm = this.fb.group({
+      surveyName: ['', Validators.required],
+      questions: this.fb.array([this.fb.control('', Validators.required)])
+    });
+  }
+
+  get questions(): FormArray {
+    return this.surveyForm.get('questions') as FormArray;
+  }
+
+  addQuestion(): void {
+    this.questions.push(new FormControl('', Validators.required));
+  }
+
+  removeQuestion(index: number): void {
+    this.questions.removeAt(index);
+    if (this.questions.length === 0) {
+      this.addQuestion();
+    }
+  }
 
   onSubmit(): void {
+    if (this.surveyForm.invalid) {
+      return;
+    }
+
     this.http
-      .post('http://localhost:9000/api/reponses', this.formData)
+      .post('http://localhost:9000/api/reponses', this.surveyForm.value)
       .subscribe({
         next: res => {
-          console.log('Réponse envoyée !', res);
-          // Réinitialiser le formulaire si besoin
-          this.formData = { nom: '', platPrefere: '' };
+          console.log('Sondage enregistré !', res);
+          this.surveyForm.reset();
+          // remettre une question vide
+          this.questions.clear();
+          this.addQuestion();
         },
-        error: err => console.error('Erreur d’envoi :', err)
+        error: err => console.error('Erreur d\u2019envoi :', err)
       });
   }
 }
--- frontend/src/app/component/footer/footer.component.ts
+++ frontend/src/app/component/footer/footer.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'footer',
-  imports: [],
+  selector: 'app-footer',
+  standalone: true,
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  styleUrls: ['./footer.component.css']
 })
-export class FooterComponent {
-
-}
+export class FooterComponent {}
